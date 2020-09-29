8d6550c1661349081e117138d2f0086714bf7184
wasahmed@student.wethinkcode.co.za <wasahmed@student.wethinkcode.co.za>
2020-09-29 14:17:23 +0200

    using a flowLayout arranges added components from left to right based on available space

--- src/gui/demo/GUIDemo.java
+++ src/gui/demo/GUIDemo.java
@@ -1,27 +1,40 @@
 package gui.demo;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUIDemo {
 
     private JFrame frame;
-    private JButton button;
+    private JTextField input;
+    private JLabel label;
+    private JButton button1;
+    private JButton button2;
     private int width;
     private int height;
 
     public GUIDemo(int width, int height) {
         frame = new JFrame();
-        button = new JButton("CLICK ME");
+        label = new JLabel("HELLO");
+        input = new JTextField(10);
+        button1 = new JButton("Button 1");
+        button2 = new JButton("Button 2");
         this.width = width;
         this.height = height;
     }
 
     public void setUpGUI(){
+        Container cp = frame.getContentPane();
+        FlowLayout flow = new FlowLayout();
+        cp.setLayout(flow);
         frame.setSize(width,height);
         frame.setTitle("First GUI");
-        frame.add(button);
+        cp.add(input);
+        cp.add(label);
+        cp.add(button1);
+        cp.add(button2);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
@@ -34,7 +47,7 @@ public class GUIDemo {
             }
         };
 
-        button.addActionListener(buttonListener);
+        button1.addActionListener(buttonListener);
     }
 }
 //Using awt and swing toolkits
